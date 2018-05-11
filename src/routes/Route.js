9454a3c1de9cc6f91a5750b9aa63acdb3016ca6b
greensirius <dominggus.octovianus@kodefox.com>
2018-05-12 04:14:10 +0700

    Update routes app

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,72 +1,25 @@
 // @flow
 
 import React from 'react';
-import {Text, View} from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
 import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
-import {StatusBar, Platform} from 'react-native';
-
-const ACTIVE_ICON_COLOR = '#FFFFFF';
-const INACTIVE_ICON_COLOR = '#666666';
-const ACTIVE_BACKGROUND_COLOR = '#6599FF';
-const NAVBAR_BACKGROUND_COLOR = 'black';
-const ICON_PRE = Platform.OS === 'ios' ? 'ios' : 'md';
-
-let Screen1 = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text>Compass</Text>
-    </View>
-  );
-};
-
-let Screen2 = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text>Achievement</Text>
-    </View>
-  );
-};
-
-let Screen3 = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text>Scanner</Text>
-    </View>
-  );
-};
+import {Ionicons} from '@expo/vector-icons';
+import {Achievement, Compass, Scaner} from '../scenes';
+import getIconPre from '../helpers/getIconPre';
+import {LIGHT_BLUE, GREY, WHITE} from '../general/colors';
 
 const TreasureStackNav = createStackNavigator(
   {
     Treasure: {
-      screen: Screen1,
+      screen: Compass,
     },
     Achievement: {
-      screen: Screen2,
+      screen: Achievement,
     },
   },
   {
     mode: 'card',
     headerMode: 'none',
-    cardStyle: {paddingTop: StatusBar.currentHeight},
+    initialRouteName: 'Treasure',
   },
 );
 
@@ -78,22 +31,22 @@ export const Tabs = createBottomTabNavigator(
         tabBarLabel: 'Compass',
         tabBarIcon: ({tintColor}: Object, focused: boolean) => (
           <Ionicons
-            name={`${ICON_PRE}-compass`}
+            name={`${getIconPre()}-compass`}
             size={20}
-            color={focused ? ACTIVE_ICON_COLOR : tintColor}
+            color={focused ? WHITE : tintColor}
           />
         ),
       },
     },
     Tab2: {
-      screen: Screen3,
+      screen: Scaner,
       navigationOptions: () => ({
         tabBarLabel: 'Scanner',
         tabBarIcon: ({tintColor}: Object, focused: boolean) => (
           <Ionicons
-            name={`${ICON_PRE}-barcode`}
+            name={`${getIconPre()}-barcode`}
             size={20}
-            color={focused ? ACTIVE_ICON_COLOR : tintColor}
+            color={focused ? WHITE : tintColor}
           />
         ),
       }),
@@ -101,21 +54,18 @@ export const Tabs = createBottomTabNavigator(
   },
   {
     tabBarOptions: {
-      activeTintColor: ACTIVE_ICON_COLOR,
-      activeBackgroundColor: ACTIVE_BACKGROUND_COLOR,
-      inactiveTintColor: INACTIVE_ICON_COLOR,
+      activeTintColor: WHITE,
+      activeBackgroundColor: LIGHT_BLUE,
+      inactiveTintColor: GREY,
       showLabel: true,
       // Android
       scrollEnabled: true,
       showIcon: true,
       indicatorStyle: {
-        backgroundColor: ACTIVE_ICON_COLOR,
-      },
-      style: {
-        height: 40,
+        backgroundColor: WHITE,
       },
       tabStyle: {
-        height: 40,
+        height: 50,
       },
     },
     tabBarPosition: 'bottom',
